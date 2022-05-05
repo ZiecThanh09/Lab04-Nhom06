14dfdebb2ee2ac6791c184be1112dc5db73dc370
UIT-20522004 <95114556+nguyenthikimthuy51@users.noreply.github.com>
2022-05-05 09:42:22 +0700

    Bai 3

---
+++ Lab04_thuy/Lab04_thuy/Lab04_Bai03.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Lab04_thuy
+{
+    partial class Lab04_Bai03
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.tbURL = new System.Windows.Forms.TextBox();
+            this.tbDesFile = new System.Windows.Forms.TextBox();
+            this.rtbContent = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(600, 15);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(122, 31);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // tbURL
+            // 
+            this.tbURL.Location = new System.Drawing.Point(42, 17);
+            this.tbURL.Name = "tbURL";
+            this.tbURL.Size = new System.Drawing.Size(495, 26);
+            this.tbURL.TabIndex = 1;
+            // 
+            // tbDesFile
+            // 
+            this.tbDesFile.Location = new System.Drawing.Point(42, 49);
+            this.tbDesFile.Name = "tbDesFile";
+            this.tbDesFile.Size = new System.Drawing.Size(495, 26);
+            this.tbDesFile.TabIndex = 2;
+            // 
+            // rtbContent
+            // 
+            this.rtbContent.Location = new System.Drawing.Point(12, 81);
+            this.rtbContent.Name = "rtbContent";
+            this.rtbContent.Size = new System.Drawing.Size(776, 357);
+            this.rtbContent.TabIndex = 3;
+            this.rtbContent.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbContent);
+            this.Controls.Add(this.tbDesFile);
+            this.Controls.Add(this.tbURL);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TextBox tbURL;
+        private System.Windows.Forms.TextBox tbDesFile;
+        private System.Windows.Forms.RichTextBox rtbContent;
+    }
+}
+
---
+++ Lab04_thuy/Lab04_thuy/Lab04_Bai03.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+namespace Lab04_thuy
+{
+    public partial class Lab04_Bai03 : Form
+    {
+        public Lab04_Bai03()
+        {
+            InitializeComponent();
+        }
+
+        public void Get_Content(WebClient myClient)
+        {
+            if (tbURL.Text.Trim() == "")
+            {
+                MessageBox.Show("Please input URL",
+                "Warning", MessageBoxButtons.OK,
+                MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                byte[] response = myClient.DownloadData(tbURL.Text);
+                rtbContent.Text = Encoding.ASCII.GetString(response);
+            }
+            catch (WebException wex)
+            {
+                rtbContent.Text = wex.Message;
+            }
+        }
+        private string OpenReader(string argv, WebClient myClient)
+        {
+            string response = "";
+            Stream strm = myClient.OpenRead(argv);
+            StreamReader sr = new StreamReader(strm);
+            while (sr.Peek() > -1)
+            {
+                response += sr.ReadLine();
+            }
+            sr.Close();
+            return response;
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            WebClient myClient = new WebClient();
+
+            try
+            {
+                myClient.DownloadFile(tbURL.Text, tbDesFile.Text);
+                OpenReader(tbDesFile.Text, myClient);
+                MessageBox.Show("File downloaded", "Information",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (WebException wex)
+            {
+                rtbContent.Text = wex.Message;
+            }
+
+            Get_Content(myClient);
+
+        }
+    } 
+}
